7acb68b7fef2073def90be578ad8e7689b4d2acb
Ivana2701 <ivanabaj@abv.bg>
2020-10-14 15:42:17 +0000

    .

---
+++ src/exercise3/PrimeNumber.java
@@ -0,0 +1,19 @@
+package exercise3;
+
+import java.util.Scanner;
+
+public class PrimeNumber {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int n = input.nextInt();
+        while (n>2){
+          
+        }
+        } 
+
+        System.out.println("End");
+    }
+
+}
