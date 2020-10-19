575c5a5a6a41307bb85747e4641a731460a1df24
Ivana2701 <ivanabaj@abv.bg>
2020-10-19 17:50:31 +0000

    ..

--- src/exercise3/AngleCalculation.java
+++ src/exercise3/AngleCalculation.java
@@ -2,6 +2,19 @@ package exersice3;
 import java.util.Scanner;
 public class AngleCalculation{
   public static void main(String[] args){
-    
+    Scanner input = input.nextDouble();
+    double angle = input.nextDouble();
+    char type = input.next().charAt(0);
+    do{
+       if(type=='r'){
+        double a=180.0*value/Math.PI;
+        System.out.print("Angle: "+a+"d");
+      }
+      else{
+        double a=value*Math.PI/180.0;
+        System.out.print("Angle: "+a+"r");
+      }
+    }
+    while(type == 'r' || 'd');
   }
 }
